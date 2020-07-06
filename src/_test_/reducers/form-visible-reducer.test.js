b7ebec93687a3ec01db3355bd7323920c110ca2e
nitunwa <nitun.2@gmail.com>
2020-07-06 16:03:38 -0700

    update

--- src/_test_/reducers/form-visible-reducer.test.js
+++ src/_test_/reducers/form-visible-reducer.test.js
@@ -1,11 +1,8 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 
-
-describe('formVisibleReducer', () => {
+describe("formVisibleReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
-    expect(formVisibleReducer(false, { type = null })).toEqual(false);
-
+    expect(formVisibleReducer(false, { type: null })).toEqual(false);
   });
-
 });
